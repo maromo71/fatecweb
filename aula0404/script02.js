d8364465f4b7603716a82c6fccdca3d2416e7587
maromo71 <professormoraes@gmail.com>
2025-08-08 07:50:48 -0300

    Primeiro commit

--- aula0404/script02.js
+++
@@ -1,6 +0,0 @@
-for(var i=0; i<10; i++){
-    setTimeout(function(){
-        console.log("Número: " + i); //repete o valor de i final 10 vezes
-    }, 1000);
-
-}
--- aula2803/exemplo02.js
+++
@@ -1,9 +0,0 @@
-let nome = "Marcos"; //global
-
-function saudacao(){
-    let mensagem = "olá"; //local
-    console.log(mensagem + ", " + nome);
-}
-
-saudacao();
-// console.log(mensagem); //dar erro
